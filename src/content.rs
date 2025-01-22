86f391001cd0e90544761eb90bc243d2052612ea
Leonard Lesinski <84378319+Le0X8@users.noreply.github.com>
2025-01-22 21:20:25 +0100

    Updated code for acr v0.4

--- src/content.rs
+++ src/content.rs
@@ -3,7 +3,11 @@ use acr::compression::decompressor::decompress;
 use dh::{Readable, Writable};
 use std::io::Result;
 
-pub fn get_content(reader: &mut dyn Readable, file: &File, buffer_size: u64) -> Result<Vec<u8>> {
+pub fn get_content<'a>(
+    reader: &'a mut dyn Readable<'a>,
+    file: &File,
+    buffer_size: u64,
+) -> Result<Vec<u8>> {
     let mut target = dh::data::write_new(file.uncompressed_size);
     decompress(
         reader,
@@ -16,9 +20,9 @@ pub fn get_content(reader: &mut dyn Readable, file: &File, buffer_size: u64) -> 
     Ok(dh::data::close(target))
 }
 
-pub fn extract_content<'a>(
-    reader: &mut dyn Readable<'a>,
-    target: &mut dyn Writable<'a>,
+pub fn extract_content<'a, W: Writable<'a>>(
+    reader: &'a mut dyn Readable<'a>,
+    target: &mut W,
     file: &File,
     buffer_size: u64,
 ) -> Result<u64> {
